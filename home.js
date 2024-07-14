08991d9732bfd98232b2dfcf1d88a67ba054ae3c
siddhartha296 <98257592+siddhartha296@users.noreply.github.com>
2024-07-14 17:46:32 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBXPNQuxkGKdLnkN7vZEWM5TkQwhq_VV54",
+    apiKey: "",
     authDomain: "health-care-management-b8.firebaseapp.com",
     projectId: "health-care-management-b8",
     storageBucket: "health-care-management-b8.appspot.com",
-    messagingSenderId: "385742303212",
-    appId: "1:385742303212:web:9fa61bb0df9dca805a1ac9",
-    measurementId: "G-6G8QW431LX"
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 firebase.initializeApp(firebaseConfig);
